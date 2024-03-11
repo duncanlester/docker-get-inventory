e6d73382a4a05f60c03939102b9e6808b8c64225
Duncan Lester <duncan.lester-2@leidos.com>
2024-03-11 10:12:33 +0000

    get inventories

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 'use strict';
 import Inventory from 'ansible-inventory-reader';
+import jp from 'jsonpath';
 
 let filepath = __dirname + '/inventories/cn1a/hosts.cn1a';
 
 let inventory = new Inventory(filepath);
-console.log(JSON.stringify(inventory, null, 2));
+var ddf_zkpr_nodes = jp.query(inventory, '$["ddf-zookeeper-nodes"].children[*].host');
+console.log(JSON.stringify(ddf_zkpr_nodes, null, 2));
