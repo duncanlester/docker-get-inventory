143a2591c03ab105fca620511a0606c91c425bb5
Duncan Lester <duncan.lester-2@leidos.com>
2024-03-11 08:23:07 +0000

    working example for inventory gets

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 'use strict';
 import Inventory from 'ansible-inventory-reader';
 
-let filepath = __dirname + '/inventories';
+let filepath = __dirname + '/inventories/cn1a/hosts.cn1a';
 
 let inventory = new Inventory(filepath);
 console.log(JSON.stringify(inventory, null, 2));
